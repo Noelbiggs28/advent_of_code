ef2caabf7642d1a4e3ff2fa70c019fb33e311f20
noelbiggs28@gmail.com <noelbiggs28@gmail.com>
2023-12-05 14:45:00 -1000

    completed part 1 of day 4

--- 2023/day_04/day_04.py
+++ 2023/day_04/day_04.py
@@ -39,31 +39,24 @@ print(part1())
 # copys also win copies. how many cards do you end up with
 def part2():
     in_file = "example.txt"
-    total_points = 0
+    total_cards = 0
+    card_amounts = {}
+    card_info = {}
     with open(in_file, "r") as infile:
         for _ in infile:
             line = _.strip()
-            # separate useful infro from card number
-            numbers = line.split(':')[1]
-            # get a list of winning numbers
-            winning_numbers = numbers.split("|")[0].strip().split(' ')
-            # get a list of your number and remove blanks spaces
-            your_numbers = numbers.split("|")[1].strip().split(' ')
-            your_numbers = [number for number in your_numbers if len(number) > 0]
-            # create a dictionary of winning numbers
-            winning_numbers_dict = {"points":0}
-            for winning_number in winning_numbers:
-                winning_numbers_dict[winning_number] = True
-            # check if your number is in the dictionary and apply math
-            for your_number in your_numbers:
-                try:
-                    if winning_numbers_dict[your_number]:
-                        if winning_numbers_dict['points'] == 0:
-                            winning_numbers_dict['points'] = 1
-                        else: 
-                            winning_numbers_dict['points'] = winning_numbers_dict['points'] * 2
-                except KeyError: 
-                    continue
-            total_points += winning_numbers_dict['points']         
-    return total_points
+            card_number = line.split(':')[0].split(" ")[1]
+            winning_numbers = line.split(":")[1].split("|")[0]
+            winning_numbers = [number for number in winning_numbers if number != ' ']
+            your_numbers = line.split((":"))[1].split("|")[1]
+            your_numbers = [number for number in your_numbers if number != ' ']
+            card_amounts[card_number] = 1
+            card_info[card_number] = (winning_numbers, your_numbers)
+           
+    current_card = 1
+    for key in card_info:
+        print(key)
+        print(card_info[key][0])
+        print(card_info[key][1])
+    return total_cards
 print(part2())
