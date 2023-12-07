c00ad31e7495787cf8b0bfc8351bedc9a34a62ae
Noelbiggs28 <noelbiggs28@gmail.com>
2023-12-06 16:18:41 -1000

    completed day 5 part 1

--- 2023/day_05/day_05.py
+++ 2023/day_05/day_05.py
@@ -1,6 +1,16 @@
 # convert until location found for each seed. find lowest location
+def check_if_between(seed, output, input, range):
+    seed, output, input, range = int(seed), int(output), int(input), int(range)
+    print(seed, output, input, range)
+    print(input, "<=", seed, "<", input+range)
+    if input <= seed < input + range:
+        return output + seed - input
+    return -1
+
+
 def part1():
-    in_file = "example.txt"
+    closest_seed = []
+    in_file = "input.txt"
     info = {}
     converter = ''
     with open(in_file, 'r') as infile:
@@ -16,6 +26,26 @@ def part1():
             elif line[-1] ==":":
                 converter = line.split(" ")[0]
                 info[converter] = []
+                continue
             info[converter].append(line.split(" "))
-    print(info)
+    for seed in info['seeds']:
+        inner = seed
+        for conversion_name in info.keys():
+            print(conversion_name)
+            if conversion_name == "seeds":
+                continue
+            for conversion in info[conversion_name]:
+                next_seed = check_if_between(inner, conversion[0], conversion[1], conversion[2])
+                if next_seed == -1:
+                    continue
+                else:
+                    inner = next_seed
+                    print(conversion_name, inner)
+                    break
+        closest_seed.append(inner)
+            
+
+            
+    print(closest_seed)
+    return min(closest_seed)
 print(part1())
