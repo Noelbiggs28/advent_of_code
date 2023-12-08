14f8281996a49a05cf845bfe874f16bd8e284516
Noelbiggs28 <noelbiggs28@gmail.com>
2023-12-07 17:21:30 -1000

    day 5 part 2. need to make it quicker

--- 2023/day_05/day_05.py
+++ 2023/day_05/day_05.py
@@ -49,3 +49,57 @@ def part1():
     # print(closest_seed)
     return min(closest_seed)
 print(part1())
+
+def part2():
+    closest_seed = [-1]
+    in_file = "input.txt"
+    info = {}
+    converter = ''
+    with open(in_file, 'r') as infile:
+        for _ in infile:
+            line = _.strip()
+            if line == '':
+                continue
+      
+            if line[:5] == "seeds":
+                line = line.split(':')
+                info[line[0]] = line[1].strip().split(' ')
+                
+                continue
+            elif line[-1] ==":":
+                converter = line.split(" ")[0]
+                info[converter] = []
+                continue
+            info[converter].append(line.split(" "))
+    for seed_info in range(0,len(info['seeds']),2):
+        print(seed_info)
+        outer = info['seeds'][seed_info]
+        # print(range(int(outer), int(outer) + int(info['seeds'][int(seed_info)+1])))
+        for seed in range(int(outer), int(outer) + int(info['seeds'][int(seed_info)+1])):
+            inner = seed
+            print(seed)
+         
+            for conversion_name in info.keys():
+                # print(conversion_name)
+                if conversion_name == "seeds":
+                    continue
+                for conversion in info[conversion_name]:
+                    next_seed = check_if_between(inner, conversion[0], conversion[1], conversion[2])
+                    if next_seed == -1:
+                        continue
+                    else:
+                        inner = next_seed
+                        # print(conversion_name, inner)
+                        break
+            if closest_seed[0] == -1:
+                closest_seed[0] = inner
+            else:
+                closest_seed.append(inner)
+                closest_seed = [min(closest_seed)]
+            
+
+            
+    # print(closest_seed)
+    return min(closest_seed)
+print(part2())
+# probably works but would take a day to compute
