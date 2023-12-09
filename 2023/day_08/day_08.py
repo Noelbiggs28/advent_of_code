66eeb42fedd5f9b7fc16f0fdee664810117eb2b1
noelbiggs28@gmail.com <noelbiggs28@gmail.com>
2023-12-08 19:12:30 -1000

    completed day 8

--- 2023/day_08/day_08.py
+++ 2023/day_08/day_08.py
@@ -1,4 +1,5 @@
 import time
+from math import lcm
 
 def part1():
     number_of_steps = 0
@@ -40,7 +41,6 @@ def part1():
 print(part1())
 
 def part2():
-    number_of_steps = 0
     in_file = "input.txt"
     map_dict = {}
     directions = ""
@@ -61,28 +61,28 @@ def part2():
                 map_dict[line[0]] = (first,second)
                 if line[0][2] == "A":
                     list_of_starting_points.append(line[0]) 
-    print(list_of_starting_points)
+
     for letter in directions:
         if letter == "L":
             directions_as_zeros_and_ones.append(0)
         else:
             directions_as_zeros_and_ones.append(1)
     
-    current_direction = directions_as_zeros_and_ones[0]
+
     amount_of_directions = len(directions_as_zeros_and_ones)
-    running = True
-    while(running):
-        still_running = False
-        for i, start in enumerate(list_of_starting_points):
-            list_of_starting_points[i] = map_dict[start][current_direction]
-            if list_of_starting_points[i][2] != "Z":
-                still_running = True
+    end_turns = []
+    for start in list_of_starting_points:
 
-        number_of_steps +=1
+        number_of_steps = 0
         current_direction =directions_as_zeros_and_ones[number_of_steps%amount_of_directions]
-        if not still_running:
-            running = False
+        current_location = start
+        while(current_location[2] != "Z"):
+            current_location = map_dict[current_location][current_direction]
+            number_of_steps +=1
+            current_direction =directions_as_zeros_and_ones[number_of_steps%amount_of_directions]
+        end_turns.append(number_of_steps)
+        answer = lcm(*end_turns)
 
-        # time.sleep(1)
-    return number_of_steps
+    return answer
 print(part2())
+# 3892373 too low
